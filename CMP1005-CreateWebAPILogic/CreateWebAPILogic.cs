2ba57f44e0db8f1ba939f60eeebc4bd32981e0f8
ryuzaki02 <thakur.aman77@yahoo.com>
2022-10-15 19:54:27 -0400

    First Commit
    Created project for WebAPILogic
    Created test project for WebAPILogic
    Added Addition logic and tests for it

---
+++ CMP1005-CreateWebAPILogic/CreateWebAPILogic.cs
@@ -0,0 +1,15 @@
+﻿namespace CMP1005_CreateWebAPILogic;
+
+public static class CreateWebAPILogic
+{
+    /*
+     * Method to Add two numbers
+     * params: left: Double , right: Double
+     * return: double
+     */
+    public static double Add(double left, double right)
+    {
+        return left + right;
+    }
+}
+
