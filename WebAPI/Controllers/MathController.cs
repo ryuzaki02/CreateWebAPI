fa1951e5ec36fa1c67551a15c7cc93534201b500
ryuzaki02 <thakur.aman77@yahoo.com>
2022-10-15 21:21:10 -0400

    Fifth Commit
    - Make your Web API perform the following task: Addition
    - Make your Web API perform the following task: Subtraction
    - Make your Web API perform the following task: Multiplication
    - Make your Web API perform the following task: Division

---
+++ WebAPI/Controllers/MathController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace WebAPI.Controllers
+{
+    [Route("api/[controller]/[action]")]
+    [ApiController]
+    public class MathController : Controller
+    {
+        [HttpGet]
+        public double Add(double left, double right)
+        {
+            return left + right;
+        }
+    }
+}
+
