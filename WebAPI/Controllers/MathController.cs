4f5b22d8a4b0e6ceb2a31e59043c3d758313ec82
ryuzaki02 <thakur.aman77@yahoo.com>
2022-10-15 21:25:59 -0400

    Added comments and removed extra file

--- WebAPI/Controllers/MathController.cs
+++ WebAPI/Controllers/MathController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using CMP1005_CreateWebAPILogic;
 
 // For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
 
@@ -12,10 +13,68 @@ namespace WebAPI.Controllers
     [ApiController]
     public class MathController : Controller
     {
+        /*
+         * Method to Add two numbers
+         * params: left: Double , right: Double
+         * return: double
+         */
         [HttpGet]
         public double Add(double left, double right)
         {
-            return left + right;
+            return CreateWebAPILogic.Add(left, right);
+        }
+
+        /*
+         * Method to Subtract two numbers
+         * params: left: Double , right: Double
+         * return: double
+         */
+        [HttpGet]
+        public double Subtract(double left, double right)
+        {
+            return CreateWebAPILogic.Subract(left, right);
+        }
+
+        /*
+         * Method to Multiply two numbers
+         * params: left: Double , right: Double
+         * return: double
+         */
+        [HttpGet]
+        public double Multiply(double left, double right)
+        {
+            return CreateWebAPILogic.Multiply(left, right);
+        }
+
+        /*
+         * Method to Divide two numbers
+         * params: left: Double , right: Double
+         * return: double
+         */
+        [HttpGet]
+        public double Divide(double left, double right)
+        {
+            double result = -1;
+            // Added try catch block to catch expection for divide by zero
+            try
+            {
+                result = CreateWebAPILogic.Divide(left, right);
+            }
+            catch (DivideByZeroException)
+            {
+                // Added status code for divide by zero expception
+                HttpContext.Response.StatusCode = 400;
+            }
+            catch (Exception)
+            {
+                // Uncaught expection
+                HttpContext.Response.StatusCode = 500;
+            }
+            finally
+            {
+                result = -1;
+            }
+            return result;
         }
     }
 }
