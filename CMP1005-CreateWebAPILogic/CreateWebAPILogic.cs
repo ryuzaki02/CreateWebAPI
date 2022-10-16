47ac895bd30dc418d14b0dc132eaf6cbc5520e23
ryuzaki02 <thakur.aman77@yahoo.com>
2022-10-15 20:27:56 -0400

    Fourth Commit
    Make your library perform the following task: Division
    Create tests for the library task: Division

--- CMP1005-CreateWebAPILogic/CreateWebAPILogic.cs
+++ CMP1005-CreateWebAPILogic/CreateWebAPILogic.cs
@@ -31,5 +31,27 @@ public static class CreateWebAPILogic
     {
         return left * right;
     }
+
+    /*
+     * Method to Divide two numbers
+     * params: left: Double , right: Double
+     * return: double
+     */
+    public static double Divide(double left, double right)
+    {
+        try
+        {
+            return left / right;
+        }
+        catch (DivideByZeroException e)
+        {
+            Console.WriteLine("Exception caught: {0}", e);
+        }
+        finally
+        {
+            Console.WriteLine("Result: {0}", 0);
+        }
+        return 0;
+    }
 }
 
